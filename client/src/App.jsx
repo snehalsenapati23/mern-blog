b34022fc51215270299db2da5ec731bb2406ea57
snehalsenapati23 <omsenapati23@gmail.com>
2024-02-17 20:05:36 +0530

    Added pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
-
-
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 function App() {
   return (
-    <>
-      <div className="text-3xl">MERN VLOG</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="sign-up" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

df7a69040d71438d99d08681a44b3d5759b128b6
snehalsenapati23 <omsenapati23@gmail.com>
2024-02-18 11:50:54 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Mongodb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(300, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running");
 });

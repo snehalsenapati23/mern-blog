7ed8d03ae1003b7b984d7efda57a1ddd24f64630
snehalsenapati23 <omsenapati23@gmail.com>
2024-02-20 01:52:16 +0530

    created signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const { User } = require("../models/user.model");
 const bcryptjs = require("bcrypt");
 const { errorHandlers } = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -12,7 +13,7 @@ const signup = async (req, res, next) => {
     email === "" ||
     password === ""
   ) {
-    next(errorHandlers(400, "All fields are required"));
+    return next(errorHandlers(400, "All fields are required"));
   }
   const hashedPassword = bcryptjs.hashSync(password, 10);
 
@@ -29,4 +30,39 @@ const signup = async (req, res, next) => {
   }
 };
 
-module.exports = { signup };
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandlers(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandlers(404, "User  not found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (validPassword) {
+      return next(errorHandlers(404, "Invalid Password"));
+    }
+
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET_KEY
+    );
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = { signup, signin };
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { signup } = require("../controllers/auth.controller");
+const { signup, signin } = require("../controllers/auth.controller");
 
 const authRoutes = express.Router();
 
 authRoutes.post("/signup", signup);
+authRoutes.post("/signin", signin);
 module.exports = { authRoutes };

c55cdda11cfcf166741ade869e9aae25e6963191
snehalsenapati23 <omsenapati23@gmail.com>
2024-02-23 02:59:24 +0530

    Dark mode functionality added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
 const Header = () => {
   const path = useLocation();
   const { currentUser } = useSelector((state) => state.user);
+  const dispath = useDispatch();
+
+  const { theme } = useSelector((state) => state.theme);
 
   return (
     <Navbar className="border-b-2">
@@ -30,8 +34,13 @@ const Header = () => {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden lg:inline" color="gray" pill>
-          <FaMoon />
+        <Button
+          onClick={() => dispath(toggleTheme())}
+          className="w-12 h-10 hidden lg:inline"
+          color="gray"
+          pill
+        >
+          {theme === "light" ? <FaMoon /> : <FaSun color="yellow" />}
         </Button>
         {currentUser ? (
           <Dropdown

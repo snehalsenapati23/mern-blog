027d04a241bd88f66b4d1963fa6fb40a08438df3
snehalsenapati23 <omsenapati23@gmail.com>
2024-02-29 00:48:58 +0530

    complete delete user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -37,25 +37,36 @@ const updateUser = async (req, res, next) => {
       );
     }
   }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-          $set: {
-            username: req.body.username,
-            email: req.body.email,
-            profilePicture: req.body.profilePicture,
-            password: req.body.password,
-          },
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
         },
-        { new: true }
-      );
-      const { password, ...rest } = updatedUser._doc;
-      res.status(200).json(rest);
-    } catch (err) {
-      next(err);
-    }
-  
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (err) {
+    next(err);
+  }
 };
 
-module.exports = { test, updateUser };
+const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandlers(403, "You are not allowed to delete this user"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { test, updateUser, deleteUser };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { test, updateUser } = require("../controllers/user.controller");
+const {
+  test,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user.controller");
 const { verifyToken } = require("../utils/verifiedUser");
 
 const userRoutes = express.Router();
 
 userRoutes.get("/test", test);
 userRoutes.put("/update/:userId", verifyToken, updateUser);
+userRoutes.delete("/delete/:userId", verifyToken, deleteUser);
 
 module.exports = { userRoutes };
